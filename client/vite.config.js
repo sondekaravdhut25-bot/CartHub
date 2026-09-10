0b56bb79be13174c4a1309dd0350f5c91d57529a
Sondekar Avdhut <sondekaravdhut25@gmail.com>
2026-09-10 19:53:49 +0530

    frontend changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "https://carthub-backend.onrender.com",
+        target: "http://localhost:5000/",
         changeOrigin: true,
       },
     },
